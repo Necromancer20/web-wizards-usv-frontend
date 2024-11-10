9ec99b3329fbbdca01043b3c257122b814997b5d
OlivianVarareanu <olivianvarareanu@gmail.com>
2024-11-10 15:02:03 +0200

    Added mock API key into .env file

--- project-frontend/src/App.jsx
+++ project-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
-  const cheie_api = 'cheie';
+  const cheie_api = import.meta.env.VITE_API_KEY;
   return (
     <>
       Cheie API: {cheie_api}
